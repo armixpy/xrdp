e28155a68046f45eb25d816dc158b7a464228b4c
armixpy <122601324+armixpy@users.noreply.github.com>
2023-01-15 19:51:25 +0330

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,11 +1,10 @@
-sudo apt update -y
+sudo apt update
 sudo apt install xubuntu-desktop -y
 sudo apt install xrdp -y
-sudo adduser xrdp ssl-cert -y
+sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
 sudo ufw allow 3389
 echo xfce4-session > $HOME/.xsession
 chmod +x .xsession
-sudo add-apt-repository ppa:martinx/xrdp-hwe-18.04 -y
-sudo apt-get update -y
 sudo apt-get install xrdp xorgxrdp -y
+sudo apt install firefox -y
